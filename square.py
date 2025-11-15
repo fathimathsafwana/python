ade055268291b8dcf6c648254eebc7051a4160e9
fathimathsafwana <fathimathsafwanatayattuchira@gmail.com>
2025-11-15 15:04:21 +0530

    Delete square.py

--- square.py
+++
@@ -1,6 +0,0 @@
-for num in range(1000, 10000):
-    root=int(num ** 0.5)
-    if root *root==num:
-        digits_even=all(int(d)%2==0 for d in str(num))
-        if digits_even:
-            print(num)
