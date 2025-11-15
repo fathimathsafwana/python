819ba65ad8ab02c49e16c727df75968c8a80ce67
fathimathsafwana <fathimathsafwanatayattuchira@gmail.com>
2025-11-15 15:15:29 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -3,4 +3,4 @@ rectangle = lambda l, b: l*b
 triangle = lambda b, h: 0.5*b*h
 print("area of square:",square(5))
 print("area of rectangle",rectangle(4,6))
-print("area of trianglr:",triangle(5,8))
+print("area of triangle:",triangle(5,8))
